97de145a0dc0cd3b6833929d65e7296c19787298
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2012-07-20 22:04:03 +0000

    CHANGED: (trivial) Endpoint Configurator: sync index.php between 2.3.0 and trunk.

--- apps/core/pbx/modules/endpoint_configurator/index.php
+++ apps/core/pbx/modules/endpoint_configurator/index.php
@@ -550,7 +550,7 @@ function network()
 
 function subMask($ip)
 {
-	$output = NULL;
+    $output = NULL;
     exec('/sbin/ip addr', $output);
     /*
     [root@picosam ~]# ip addr show
@@ -567,9 +567,9 @@ function subMask($ip)
      */
     foreach ($output as $s) {
         $regs = NULL;
-    	if (preg_match('|inet (\d+.\d+.\d+.\d+)/(\d+)|', $s, $regs)) {
-    		if ($regs[1] == $ip) return (int)$regs[2];
-    	}
+        if (preg_match('|inet (\d+.\d+.\d+.\d+)/(\d+)|', $s, $regs)) {
+            if ($regs[1] == $ip) return (int)$regs[2];
+        }
     }
     return 32;  // No se pudo encontrar máscara de la red
 }

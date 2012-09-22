6cbde6fee25ceff27fa9512c0b52b080a791bc9a
labarca2909 <labarca2909@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2012-09-22 15:38:20 +0000

    ADDED: Function load_default_timezone for resolve a warning launched by date function.

--- apps/core/pbx/setup/asterisk/agi-bin/phpagi-asmanager.php
+++ apps/core/pbx/setup/asterisk/agi-bin/phpagi-asmanager.php
@@ -30,7 +30,10 @@
   {
     require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'phpagi.php');
   }
-
+  
+  require_once "/var/www/html/libs/misc.lib.php";
+  load_default_timezone();
+  
  /**
   * Asterisk Manager class
   *

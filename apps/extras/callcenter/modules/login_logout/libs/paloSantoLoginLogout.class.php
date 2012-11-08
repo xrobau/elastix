1a0fd3f34725bdc3994e0a51552cb70896d0e32c
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2012-11-08 21:41:40 +0000

    CHANGED: Login Logout: complete rewrite. This rewrite takes advantage of the updated Elastix API for paloSantoGrid available since elastix-framework-2.2-18. The custom code for calendars was replaced with standard date controls. Most importantly, the SQL query was considerably simplified for readability, and in the process, fixes the calculation of total session time. Finally, the filtering by incoming queue from Agents Connection Time was added to this module. Part of fix for Elastix bug #1409.

--- apps/extras/callcenter/modules/login_logout/libs/paloSantoLoginLogout.class.php
+++ apps/extras/callcenter/modules/login_logout/libs/paloSantoLoginLogout.class.php
@@ -27,12 +27,13 @@
   +----------------------------------------------------------------------+
   $Id: new_campaign.php $ */
 
-include_once("libs/paloSantoDB.class.php");
+require_once('libs/paloSantoDB.class.php');
+require_once 'libs/paloSantoConfig.class.php';
+include_once 'libs/paloSantoQueue.class.php';
 
-/* Clase que implementa campaña (saliente por ahora) de CallCenter (CC) */
 class paloSantoLoginLogout
 {
-    var $_DB; // instancia de la clase paloDB
+    private $_DB; // instancia de la clase paloDB
     var $errMsg;
 
     function paloSantoLoginLogout(&$pDB)
@@ -54,206 +55,112 @@ class paloSantoLoginLogout
         }
     }
 
-
-
-     //Procedimiento para obtener el número de llamadas entrantes agrupadas por colas
-
-    function getRegistersLoginLogout($tipo='all',$fecha_init,$fecha_final,$limit, $offset)
+    function leerColasEntrantesValidas()
     {
-        global $arrLang;
-        global $arrLangModule;
+        //conexion resource
+        $pConfig = new paloConfig("/etc", "amportal.conf", "=", "[[:space:]]*=[[:space:]]*");
+        $arrConfig = $pConfig->leer_configuracion(false);
+        $dsnAsteriskCdr = $arrConfig['AMPDBENGINE']['valor']."://".
+                          $arrConfig['AMPDBUSER']['valor']. ":".
+                          $arrConfig['AMPDBPASS']['valor']. "@".
+                          $arrConfig['AMPDBHOST']['valor']."/asterisk";
+        $pDB_asterisk = new paloDB($dsnAsteriskCdr);
+        $oQueue  = new paloQueue($pDB_asterisk);
+        $PBXQueues = $oQueue->getQueue();
 
-        //validamos la fecha
-        if($fecha_init!="" && $fecha_final !="" ) {
-            $fecha_init = explode('-',$fecha_init);
-            $fecha_final = explode('-',$fecha_final);
-        }else {
-            $this->msgError .= "Debe ingresarse una fecha inicio y una fecha fin";
-            return false;
+        $arrQueue = array();
+        if (is_array($PBXQueues)) {
+            foreach($PBXQueues as $key => $value) {
+                $result = $this->_DB->getFirstRowQuery(
+                    'SELECT id, queue from queue_call_entry where queue = ?',
+                    TRUE, array($value[0]));
+                if (is_array($result) && count($result)>0) {
+                    // La clave debe ser cadena para que in_array en paloForm funcione
+                    $arrQueue[$result['id']] =  $result['queue'];
+                }
+            }
         }
-        // pregunto si la fecha inicial existe
-        if( is_array( $fecha_init ) && count( $fecha_init )==3 ) {
-            $year_init  = $fecha_init[0];
-            $month_init = $fecha_init[1];
-            $day_init   = $fecha_init[2];
-            $fechaInicial = $fecha_init[0]."-".$fecha_init[1]."-".$fecha_init[2]." "."00:00:00";
-            //$fechaFinal = $fecha_init[0]."-".$fecha_init[1]."-".$fecha_init[2]." "."23:59:59";
-            $fechaFinal = $fecha_final[0]."-".$fecha_final[1]."-".$fecha_final[2]." "."23:59:59";
-        //si fecha_init y fecha_end no existen envio un mensaje de error
-        }else {
-            $this->msgError .= "Fecha Inicio y/o Fecha Fin no valida";
-            return false;
+        return $arrQueue;
+    }
+    
+    function leerRegistrosLoginLogout($sTipo, $sFechaInicio, $sFechaFin, $idIncomingQueue = NULL)
+    {
+        if (!in_array($sTipo, array('D', 'G'))) {
+            $this->errMsg = _tr('(internal) Invalid detail flag, must be D or G');
+        	return NULL;
         }
-
-        $arreglo = array();
-        //para filtrar por fechas
-        $where = " AND datetime_init between '{$fechaInicial}' AND '{$fechaFinal}'  ";
-        if($tipo=='D'){//hacemos consulta DETALLADA
-
-            $arr_result = FALSE;
-            $this->errMsg = "";
-
-            $sPeticionSQL = "SELECT
-                agent.number as number,
-                agent.name as name,
-                a.id,
-                agent.name as name,
-                (a.datetime_init) as datetime_init,
-                if((a.datetime_end) is null, now(), a.datetime_end) as datetime_end,
-                TIME_TO_SEC(TIMEDIFF((if((a.datetime_end) is null, now(), a.datetime_end)),(datetime_init))) as total_sesion,
-                ((
-                    if((select sum(ce.duration) from call_entry as ce where ce.datetime_init between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end)) and ce.id_agent=agent.id) is null, 0, 
-                    (select sum(ce.duration) from call_entry as ce where ce.datetime_init between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end)) and ce.id_agent=agent.id)
-                    ) +
-                    if((select sum(duration) from calls where start_time between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end)) and id_agent=agent.id) is null,0,(select sum(duration) from calls where start_time between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end)) and id_agent=agent.id ))
-                    ) / 
-                    TIME_TO_SEC(TIMEDIFF((if((a.datetime_end)is null,now(),a.datetime_end)),a.datetime_init))
-                ) * 100 as service,
-                
-                (
-                        if((select sum(duration) from call_entry where datetime_init between a.datetime_init and  (if((a.datetime_end)is null,now(),a.datetime_end)) and id_agent=agent.id) is null, 0, (select sum(duration) from call_entry where datetime_init between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end))  and id_agent=agent.id)
-                        ) +
-                        if((select sum(duration) from calls where start_time between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end)) and id_agent=agent.id) is null, 0 , (select sum(duration) from calls where start_time between a.datetime_init and (if((a.datetime_end)is null,now(),a.datetime_end)) and id_agent=agent.id))
-                ) as total_sumas_in_out,
-                if((a.datetime_end) is null, 'En Linea', '') as estado 
-                FROM 
-                    audit a, 
-                    agent 
-                WHERE a.datetime_init between '{$fechaInicial}' AND '{$fechaFinal}'                
-                        and a.id_agent = agent.id and id_break is null
-		ORDER BY agent.name, a.datetime_init" ;
-
+        $sRegexp = '/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/';
+        if (!preg_match($sRegexp, $sFechaInicio)) {
+            $this->errMsg = _tr('(internal) Invalid start date, must be yyyy-mm-dd hh:mm:ss');
+        	return NULL;
         }
-        else if($tipo=='G'){//hacemos consulta GENERAL
-            $sPeticionSQL =  "SELECT
-    agent.number as number,
+        if (!preg_match($sRegexp, $sFechaFin)) {
+            $this->errMsg = _tr('(internal) Invalid end date, must be yyyy-mm-dd hh:mm:ss');
+            return NULL;
+        }
+        if ($sFechaFin < $sFechaInicio) {
+        	$t = $sFechaFin;
+            $sFechaFin = $sFechaInicio;
+            $sFechaInicio = $t;
+        }
+    	$sqlRegistros = <<<SQL_REGISTROS
+SELECT
     agent.id,
+    agent.number,
     agent.name,
-    min(audit.datetime_init) as datetime_init,
-
-    if (
-        (select count(au.datetime_end) from audit au, agent ag where au.datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and au.id_agent=agent.id and au.datetime_end is null group by au.id_agent) is null,
-        max(audit.datetime_end),
-        now()
-    ) as datetime_end,
-
+    audit.datetime_init, 
+    IF(audit.datetime_end IS NULL, NOW(), datetime_end) AS datetime_end,
+    TIME_TO_SEC(IF(audit.duration IS NULL, TIMEDIFF(NOW(), audit.datetime_init), audit.duration)) AS duration,
     (
-        if(
-            (select sum(duration) from call_entry where datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and call_entry.id_agent=agent.id) is null,
-            0,
-            (select sum(duration) from call_entry where datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and call_entry.id_agent=agent.id)
-        ) +
-        if(
-            (select sum(duration) from calls where start_time between '{$fechaInicial}' AND '{$fechaFinal}' and calls.id_agent=agent.id)is null,
-            0,
-            (select sum(duration) from calls where start_time between '{$fechaInicial}' AND '{$fechaFinal}' and calls.id_agent=agent.id)
-        )
-    ) as  total_sumas_in_out,
-
+        SELECT SUM(duration) FROM call_entry
+        WHERE   call_entry.id_agent = agent.id
+            AND (? IS NULL OR call_entry.id_queue_call_entry = ?)
+            AND call_entry.datetime_init BETWEEN audit.datetime_init AND IF(audit.datetime_end IS NULL, NOW(), audit.datetime_end)
+    ) AS total_incoming,
     (
-        TIME_TO_SEC(
-            sec_to_time(
-                if(
-                    (select sum(duration) from call_entry where datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and call_entry.id_agent=agent.id) is null,
-                    0,
-                    (select sum(duration) from call_entry where datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and call_entry.id_agent=agent.id)
-                ) +
-                if(
-                    (select sum(duration) from calls where start_time between '{$fechaInicial}' AND '{$fechaFinal}' and calls.id_agent=agent.id)is null,
-                    0,
-                    (select sum(duration) from calls where start_time between '{$fechaInicial}' AND '{$fechaFinal}' and calls.id_agent=agent.id)
-                )
-            )
-        )
-        /
-        TIME_TO_SEC(
-            TIMEDIFF(
-                if (
-                    (select count(au.datetime_end) from audit au, agent ag where au.datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and au.id_agent=agent.id and au.datetime_end is null group by au.id_agent) is null,
-                    max(audit.datetime_end),
-                    now()
-                ),
-                min(audit.datetime_init)
-            )
-        )
-    ) * 100 as service,
-
-    TIME_TO_SEC(TIMEDIFF(
-        if (
-            (select count(au.datetime_end) from audit au, agent ag where au.datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and au.id_agent=agent.id and au.datetime_end is null group by au.id_agent) is null,
-            max(audit.datetime_end),
-            now()
-        ),
-        min(audit.datetime_init)
-    ))  as total_sesion,
-
-    if (
-        (select count(au.datetime_end) from audit au, agent ag where au.datetime_init between '{$fechaInicial}' AND '{$fechaFinal}' and au.id_agent=agent.id and au.datetime_end is null group by au.id_agent) is null,
-        '',
-        'En linea'
-    ) as estado
-
-FROM
-    audit, agent
-
-WHERE
-    audit.id_agent=agent.id
-    and audit.datetime_init between '{$fechaInicial}' AND '{$fechaFinal}'
-    AND audit.id_break is null
-
-GROUP BY agent.id
-
-ORDER BY agent.name";
-
-//echo $sPeticionSQL."<br>";
-
-        }
-        if(!empty($limit)) {
-	        $sPeticionSQL  .= " LIMIT $limit OFFSET $offset";
-        }
-
-//echo $sPeticionSQL."<br><br>";
-
-        $arr_result =& $this->_DB->fetchTable($sPeticionSQL, true);
-        if (!is_array($arr_result)) {
-            $arr_result = FALSE;
+        SELECT SUM(duration) FROM calls
+        WHERE   calls.id_agent = agent.id 
+            AND calls.start_time BETWEEN audit.datetime_init AND IF(audit.datetime_end IS NULL, NOW(), audit.datetime_end)
+    ) AS total_outgoing,
+    IF(audit.datetime_end IS null, 'ONLINE', '') AS estado 
+FROM audit, agent
+WHERE   audit.datetime_init BETWEEN ? AND ?
+    AND audit.id_agent = agent.id
+    AND audit.id_break IS NULL
+ORDER BY agent.name, audit.datetime_init
+SQL_REGISTROS;
+        $recordset = $this->_DB->fetchTable($sqlRegistros, TRUE,
+            array($idIncomingQueue, $idIncomingQueue, $sFechaInicio, $sFechaFin));
+        if (!is_array($recordset)) {
             $this->errMsg = $this->_DB->errMsg;
+        	return NULL;
         }
-
-//print_r($arr_result);
-
-
-    $arrResult['Data'] = $arr_result;//toda la data
-    $arrResult['NumRecords'] = count($arrResult['Data']); //contabilizamos la cantidad de datos
-
-    return $arrResult;//retorno el arreglo
-
+        foreach (array_keys($recordset) as $i) {
+        	if (is_null($recordset[$i]['total_incoming']))
+                $recordset[$i]['total_incoming'] = 0;
+            if (is_null($recordset[$i]['total_outgoing']))
+                $recordset[$i]['total_outgoing'] = 0;
+        }
+        
+        // Para reporte detallado, ya se puede devolver recordset
+        if ($sTipo == 'D') return $recordset;
+        
+        // Agrupar por ID de agente para reporte general
+        $agrupacion = array();
+        foreach ($recordset as $tupla) {
+        	if (!isset($agrupacion[$tupla['id']])) {
+        		$agrupacion[$tupla['id']] = $tupla;
+        	} else {
+        		foreach (array('duration', 'total_incoming', 'total_outgoing') as $k)
+                    $agrupacion[$tupla['id']][$k] += $tupla[$k];
+                if ($agrupacion[$tupla['id']]['datetime_init'] > $tupla['datetime_init'])
+                    $agrupacion[$tupla['id']]['datetime_init'] = $tupla['datetime_init'];
+                if ($agrupacion[$tupla['id']]['datetime_end'] < $tupla['datetime_end'])
+                    $agrupacion[$tupla['id']]['datetime_end'] = $tupla['datetime_end'];
+                $agrupacion[$tupla['id']]['estado'] = $tupla['estado'];
+        	}
+        }
+        return $agrupacion;
     }
-
-    /*
-        Esta funcion recibe un dos tiempos y retorna la suma de ellos
-    */
-    function getSumTime($time1,$time2) {
-
- 	if( is_null($time1) ) {
- 	    $time1 = "00:00:00";
- 	}
- 	if( is_null($time2) ) {
- 	    $time2 = "00:00:00";
- 	}
- 
- 	$SQLConsulta = "select addtime('{$time1}','{$time2}') duracion";
- 	$resConsulta = $this->_DB->getFirstRowQuery($SQLConsulta,true); 
-
- 	if(!$resConsulta)  {
-             $this->msgError = $this->errMsg;
- 	    return false;
- 	} else {
- 	   return $resConsulta['duracion'];
- 	}
-    }
-
-
 }
-
 ?>

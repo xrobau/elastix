1e5c4e9018b902a07ddc0fed8e7a5d594701980d
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2012-11-16 16:19:19 +0000

    CHANGED: Build Module: create new privileged script develbuilder and reimplement module creation using this script. Required to cope with recent security changes that switch ownership of Elastix GUI to root. As a side effect, we get a net reduction of code size and some improvement in readability. CHANGED: Developer: switched module ownership to root.root.

--- apps/extras/developer/modules/build_module/libs/paloSantoBuildModule.class.php
+++ apps/extras/developer/modules/build_module/libs/paloSantoBuildModule.class.php
@@ -110,308 +110,69 @@ class paloSantoBuildModule {
         else return true;
     }
 
-    function Query_Elastix_Version()
+    private function Query_Elastix_Version()
     {
         $query = "SELECT value FROM settings WHERE key='elastix_version_release'";
         $result = $this->_DB->getFirstRowQuery($query);
         return $result[0];
     }
 
-    function Create_File_Config($new_id_module, $your_name, $ruta, $elastix_Version, $arrLang, $this_module_name, $email_module)
+    /**
+     * Procedimiento para crear la estructura de archivos de un nuevo módulo.
+     * 
+     * @param   string  $sModuleID  ID del módulo a guardar, y directorio base
+     * @param   string  $sModuleName    Nombre del módulo que aparece en GUI
+     * @param   string  $sAuthorName    Nombre del autor, se incluye en cabeceras
+     * @param   string  $sEmail         Correo electrónico del autor
+     * @param   string  $sModuleType    grid|form
+     * @param   array   $fieldList      Para grid, lista de etiquetas de columnas 
+     *                                  a crear. Para form, lista de tuplas de 
+     *                                  (etiqueta, tipo).
+     * 
+     * @return  bool    VERDADERO en éxito, FALSO en error
+     */
+    function createModuleFiles($sModuleId, $sModuleName, $sAuthorName, $sEmail, $sModuleType, $fieldList)
     {
-        $filename = 'default.conf.php';
-        $date = date('Y-m-d h:m:s');
-
-        $file = "$ruta/$this_module_name/libs/sources/comment.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
+        if (!in_array($sModuleType, array('grid', 'form'))) {
+            $this->errMsg = '(internal) Unsupported module type, must be grid or form';
+        	return FALSE;
         }
-        $content = fread($gestor, filesize($file));
-        $content = str_replace("{ELASTIX_VERSION}", "$elastix_Version", $content);
-        $content = str_replace("{DATE}", "$date", $content);
-        $content = str_replace("{YOUR_NAME}", "$your_name", $content);
-        $content = str_replace("{FILE_NAME}", "$filename", $content);
-        $content = str_replace("{YOUR_EMAIL}", "$email_module", $content);
-        fclose($gestor);
-
-        $contenido = "<?php\n$content";
-
-        $file = "$ruta/$this_module_name/libs/sources/default.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $content = fread($gestor, filesize($file));
-        $content = str_replace("{MODULE_ID}", "$new_id_module", $content);
-        fclose($gestor);
-
-        $contenido .= "$content?>";
-
-        $ruta .= "/$new_id_module/configs";
-        return $this->Create_File($ruta, $filename, $contenido, $arrLang);
-    }
-
-    function Create_File_Help($new_id_module, $your_name, $ruta, $elastix_Version, $arrLang, $this_module_name)
-    {
-        $filename = "$new_id_module.hlp";
-
-        $file = "$ruta/$this_module_name/libs/sources/help.tpl";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $contenido = fread($gestor, filesize($file));
-        fclose($gestor);
-
-        $ruta .= "/$new_id_module/help";
-        return $this->Create_File($ruta, $filename, $contenido, $arrLang);
-    }
-
-    function Create_File_Lang($new_module_name, $new_id_module, $your_name, $ruta, $elastix_Version, $arrLang, $this_module_name, $arrForm, $email_module)
-    {
-        $filename = 'en.lang';
-        $date = date('Y-m-d h:m:s');
-
-        $file = "$ruta/$this_module_name/libs/sources/comment.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $content = fread($gestor, filesize($file));
-        $content = str_replace("{ELASTIX_VERSION}", "$elastix_Version", $content);
-        $content = str_replace("{DATE}", "$date", $content);
-        $content = str_replace("{YOUR_NAME}", "$your_name", $content);
-        $content = str_replace("{FILE_NAME}", "$filename", $content);
-        $content = str_replace("{YOUR_EMAIL}", "$email_module", $content);
-        fclose($gestor);
-
-        $contenido = "<?php\n$content";
-
-        $file = "$ruta/$this_module_name/libs/sources/lang.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $content = fread($gestor, filesize($file));
-
-        if(is_array($arrForm) && count($arrForm) >0){
-	    $lang_module_name = str_replace('"','\"',$new_module_name);
-	    $lang_module_name = str_replace('$','\$',$lang_module_name);
-            $tmpLanguage = "\"$lang_module_name\" => \"$lang_module_name\",";
-            foreach($arrForm as $key => $names){
-                $arrName = explode("/",$names);
-		$translation = str_replace('"','\"',$arrName[0]);
-		$translation = str_replace('$','\$',$translation);
-                $tmpLanguage .= "\n\"$translation\" => \"$translation\",";
+        
+    	// Generación de archivo XML temporal para script privilegiado
+        $xml_modulespec = new SimpleXMLElement('<modulespec />');
+        $xml_modulespec->addChild('name', str_replace('&', '&amp;', $sModuleName));
+        $xml_modulespec->addChild('id', $sModuleId);
+        $xml_modulespec->addChild('author', str_replace('&', '&amp;', $sAuthorName));
+        $xml_modulespec->addChild('email', str_replace('&', '&amp;', $sEmail));
+        $xml_modulespec->addChild('elastixversion', $this->Query_Elastix_Version());
+        $xml_fields = $xml_modulespec->addChild($sModuleType);
+        if ($sModuleType == 'grid') {
+        	foreach ($fieldList as $k => $field) {
+                $xml_field = $xml_fields->addChild('column', str_replace('&', '&amp;', $field));
+                $xml_field->addAttribute('key', $k);
+            }
+        } elseif ($sModuleType == 'form') {
+            foreach ($fieldList as $field) {
+                $xml_field = $xml_fields->addChild('field', str_replace('&', '&amp;', $field[0]));
+                $xml_field->addAttribute('type', $field[1]);
             }
         }
-        $content = str_replace("{LANG_CONTENT}",$tmpLanguage, $content);
-        fclose($gestor);
-
-        $contenido .= "$content?>";
-
-        $ruta .= "/$new_id_module/lang";
-        return $this->Create_File($ruta, $filename, $contenido, $arrLang);
-    }
-
-    function Create_Module_Class_File($new_module_name, $new_id_module, $your_name, $ruta, $elastix_Version, $arrLang, $this_module_name, $email_module)
-    {
-	$name_class = preg_replace("/\W/","_",$new_module_name);
-	$name_class = preg_replace("/_+/","_",$name_class);
-	$name_class = preg_replace("/_$/","",$name_class);
-        $filename = "paloSanto$name_class.class.php";
-        $date = date('Y-m-d h:m:s');
-
-        $file = "$ruta/$this_module_name/libs/sources/comment.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
+        $sTempFile = tempnam('/tmp', 'modulespec_');
+        if (!$xml_modulespec->asXML($sTempFile)) {
+            $this->errMsg = '(internal) Failed to write temporary module specification';
+        	return FALSE;
         }
-        $content = fread($gestor, filesize($file));
-        $content = str_replace("{ELASTIX_VERSION}", "$elastix_Version", $content);
-        $content = str_replace("{DATE}", "$date", $content);
-        $content = str_replace("{YOUR_NAME}", "$your_name", $content);
-        $content = str_replace("{FILE_NAME}", "$filename", $content);
-        $content = str_replace("{YOUR_EMAIL}", "$email_module", $content);
-        fclose($gestor);
-
-        $contenido = "<?php\n$content";
-
-        $file = "$ruta/$this_module_name/libs/sources/lib_class.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
+        
+        // Invocación del script privilegiado
+        $output = $retval = NULL;
+        exec('/usr/bin/elastix-helper develbuilder --createmodule '.escapeshellarg($sTempFile).' 2>&1',
+            $output, $retval);
+        unlink($sTempFile);
+        if ($retval != 0) {
+            $this->errMsg = '(internal) Failed to create module files - '.implode('<br/>', $output);
+        	return FALSE;
         }
-        $content = fread($gestor, filesize($file));
-        $content = str_replace("{NAME_CLASS}", "$name_class", $content);
-        fclose($gestor);
-
-        $contenido .= "$content?>";
-
-        $ruta .= "/$new_id_module/libs";
-        return $this->Create_File($ruta, $filename, $contenido, $arrLang);
-    }
-
-    function Create_Index_File($new_module_name, $new_id_module, $your_name, $ruta, $elastix_Version, $arrLang, $type, $this_module_name,$arrForm, $email_module)
-    {
-        $name_class = preg_replace("/\W/","_",$new_module_name);
-	$name_class = preg_replace("/_+/","_",$name_class);
-	$name_class = preg_replace("/_$/","",$name_class);
-	$translateModuleName = str_replace('"','\"',$new_module_name);
-	$translateModuleName = str_replace('$','\$',$translateModuleName);
-        $filename = "index.php";
-        $date = date('Y-m-d h:m:s');
-        $field = array();
-        $content_form = "";
-        $content_file = "";
-
-        $file = "$ruta/$this_module_name/libs/sources/comment.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $content = fread($gestor, filesize($file));
-        $content = str_replace("{ELASTIX_VERSION}", "$elastix_Version", $content);
-        $content = str_replace("{DATE}", "$date", $content);
-        $content = str_replace("{YOUR_NAME}", "$your_name", $content);
-        $content = str_replace("{FILE_NAME}", "$filename", $content);
-        $content = str_replace("{YOUR_EMAIL}", "$email_module", $content);
-        fclose($gestor);
-
-        $contenido = "<?php\n$content";
-
-        $file = "$ruta/$this_module_name/libs/sources/index_$type.s";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $content = fread($gestor, filesize($file));
-
-        if($type == "grid"){
-            $blockRows    ="";
-            $blockColumns ="";
-            $blockFilters ="";
-            if(is_array($arrForm) && count($arrForm)>0){
-                foreach($arrForm as $key => $column){
-                    //Para crear las lineas de los datos rows en el grid.
-                    $tmpNameColumn = preg_replace("/\W/","_",strtolower($column));
-		    $tmpNameColumn = preg_replace("/_+/","_",$tmpNameColumn);
-		    $tmpNameColumn = preg_replace("/_$/","",$tmpNameColumn);
-		    $translateColumn = str_replace('"','\"',$column);
-		    $translateColumn = str_replace('$','\$',$translateColumn);
-                    $blockRows .= "\n\t    \$arrTmp[$key] = \$value['$tmpNameColumn'];";
-                    //Para crear las lineas de las columnas en el grid.
-                    $blockColumns .= "_tr(\"$translateColumn\"),";
-                    //Para crear las lineas del arreglo para el fitrado o busqueda.
-                    $blockFilters .= "\n\t    \"$tmpNameColumn\" => _tr(\"$translateColumn\"),";
-                }
-                $content = str_replace("{ARR_DATA_ROWS}",$blockRows,$content);
-                $content = str_replace("{ARR_NAME_COLUMNS}",$blockColumns,$content);
-                $content = str_replace("{ARR_FILTERS}",$blockFilters,$content);
-            }
-        }else{
-             if(is_array($arrForm) && count($arrForm) >0){
-                    foreach($arrForm as $key => $value){
-                        $field = explode("/",$value);
-			$field[1] = rtrim($field[1]);
-                        if(file_exists("$ruta/$this_module_name/libs/sources/fields_form/$field[1].s")){
-                            $file_form = "$ruta/$this_module_name/libs/sources/fields_form/$field[1].s";
-                                if (!$gestor_form = fopen($file_form, 'r')) {
-                                    $this->errMsg = $arrLang["It isn't possible to open file FORM for reading"]. ": $file";
-                                    return false;
-                                }
-                                $content_file = fread($gestor_form, filesize($file_form));
-				$tmpfield = str_replace('"','\"',$field[0]);
-				$tmpfield = str_replace('$','\$',$tmpfield);
-				$tmpNameField =  preg_replace("/\W/","_",strtolower(trim($field[0])));
-				$tmpNameField =  preg_replace("/_+/","_",$tmpNameField);
-				$tmpNameField =  preg_replace("/_$/","",$tmpNameField);
-                                $content_file = str_replace("{LABEL_FIELD}", $tmpfield, $content_file);
-                                $content_file = str_replace("{NAME_FIELD}", $tmpNameField, $content_file);
-                                $content_form .= $content_file;
-                                fclose($gestor_form);
-                            }
-                    }
-                    $content = str_replace("{ARR_FIELDS_FORM}", "$content_form", $content);
-              }
-        }
-
-
-        $content = str_replace("{NAME_CLASS}", "$name_class", $content);
-        $content = str_replace("{NEW_MODULE_NAME}", "$translateModuleName", $content);
-
-        fclose($gestor);
-
-        $contenido .= "$content?>";
-
-        $ruta .= "/$new_id_module";
-        return $this->Create_File($ruta, $filename, $contenido, $arrLang);
-    }
-
-    function Create_tpl_File($new_id_module, $ruta, $arrLang, $type, $this_module_name, $arrForm)
-    {
-        $field = array();
-        $content_form = "";
-		$content_file = "";
-        $filename = "";
-        //primero hay q leer el archivo form.tpl y asignar a alguna variable despues a esta varaible
-       //concatenamos el cuerpo es decir los tr que contienen los elementos form
-        //$content_form .= $content_file;
-        if($type=="grid"){
-            $filename = "filter.tpl";
-            $file = "$ruta/$this_module_name/libs/sources/filter.tpl";
-        }
-        else /*if($type == "form")*/{
-            $filename = "form.tpl";
-            $file = "$ruta/$this_module_name/libs/sources/form.tpl";
-            if(is_array($arrForm) && count($arrForm) >0){
-			   if(file_exists("$ruta/$this_module_name/libs/sources/fields_form/fields_form.tpl")){
-		               	$file_form = "$ruta/$this_module_name/libs/sources/fields_form/fields_form.tpl";					
-
-                foreach($arrForm as $key => $value){
-                        $field = explode("/",$value);
-                        if (!$gestor_form = fopen($file_form, 'r')) {
-                            $this->errMsg = $arrLang["It isn't possible to open file FORM for reading"]. ": $file";
-                            return false;
-                        }
-			$tmpFieldLabel = preg_replace("/\W/","_",strtolower(trim($field[0])));
-			$tmpFieldLabel = preg_replace("/_+/","_",$tmpFieldLabel);
-			$tmpFieldLabel = preg_replace("/_$/","",$tmpFieldLabel);
-                        $content_file = fread($gestor_form, filesize($file_form));
-                        $content_file = str_replace("{FIELD_LABEL}", $tmpFieldLabel, $content_file);
-			$content_form .= $content_file;
-                        fclose($gestor_form);
-                    }
-                }
-            }
-        }
-        $form_tpl_file = "$ruta/$this_module_name/libs/sources/index_$type.tpl";
-        if (!$gestor = fopen($file, 'r')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for reading"]. ": $file";
-            return false;
-        }
-        $contenido = fread($gestor, filesize($file));
-        $contenido = str_replace("{FIELDS_FORM}",$content_form, $contenido);
-        fclose($gestor);
-
-        $ruta .= "/$new_id_module/themes/default";
-        return $this->Create_File($ruta, $filename, $contenido, $arrLang);
-    }
-
-    function Create_File($ruta, $filename, $contenido, $arrLang)
-    {
-        if (!$gestor = fopen("$ruta/$filename", 'w')) {
-            $this->errMsg = $arrLang["It isn't possible to open file for writing"]. ": $filename";
-            return false;
-        }
-        // Escribir $contenido a nuestro archivo abierto.
-        if (fwrite($gestor, $contenido) === FALSE) {
-            $this->errMsg = $arrLang["Error when writing file"]. ": $filename";
-            return false;
-        }
-        fclose($gestor);
-        return true;
+        return TRUE;
     }
 }
 ?>

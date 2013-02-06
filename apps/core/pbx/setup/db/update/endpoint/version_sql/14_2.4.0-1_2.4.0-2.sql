7bc8c9355d3f7a85fd8f560bc97546966511241c
gmacas <gmacas@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-02-06 21:43:24 +0000

    ADD: modules: endpoint_configurator: Add support to set new Fanvil models C56/C56P C58/C58P and C60

--- apps/core/pbx/setup/db/update/endpoint/version_sql/14_2.4.0-1_2.4.0-2.sql
+++ apps/core/pbx/setup/db/update/endpoint/version_sql/14_2.4.0-1_2.4.0-2.sql
@@ -1,1 +1,4 @@
 insert into model (name,description,id_vendor,iax_support)values("SIP-T38G","SIP-T38G",(select id from vendor where name="Yealink"),'0');
+insert into model (name,description,id_vendor,iax_support)values("C60","C60",(select id from vendor where name="Fanvil"),'1');
+insert into model (name,description,id_vendor,iax_support)values("C58/C58P","C58/C58P",(select id from vendor where name="Fanvil"),'1');
+insert into model (name,description,id_vendor,iax_support)values("C56/C56P","C56/C56P",(select id from vendor where name="Fanvil"),'0');

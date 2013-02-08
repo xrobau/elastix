dfafb5f6b911b1c53159625f035d0d22b6592c12
gmacas <gmacas@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-02-08 20:39:57 +0000

    ADD: modules: endpoint_configurator: Add support to set new model Snom 821

--- apps/core/pbx/setup/db/update/endpoint/version_sql/14_2.4.0-1_2.4.0-2.sql
+++ apps/core/pbx/setup/db/update/endpoint/version_sql/14_2.4.0-1_2.4.0-2.sql
@@ -2,3 +2,4 @@ insert into model (name,description,id_vendor,iax_support)values("SIP-T38G","SIP
 insert into model (name,description,id_vendor,iax_support)values("C60","C60",(select id from vendor where name="Fanvil"),'1');
 insert into model (name,description,id_vendor,iax_support)values("C58/C58P","C58/C58P",(select id from vendor where name="Fanvil"),'1');
 insert into model (name,description,id_vendor,iax_support)values("C56/C56P","C56/C56P",(select id from vendor where name="Fanvil"),'0');
+insert into model (name,description,id_vendor,iax_support)values("821","snom821-SIP",(select id from vendor where name="Snom"),'0');

449996a0708655b969596c9c51b715ff93c130f5
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-07-15 20:15:02 +0000

    CHANGED: Dashboard: complete rewrite. The dashboard module has been completely rewritten in order to clean up the code and in order to modularize the applets and introduce the capability for a third-party addon to define a custom dashboard applet. A lot of cruft was removed, several potential SQL injection vulnerabilities were closed, and all applets were reviewed in the process of reorganization. Some highlights:   All applets: most of the HTML embedded in the source code was moved to templates.   SystemResources: a long-standing design flaw was fixed. Previously the CPU gauge would only show the CPU load for a small interval while the applet was being rendered, with the result that it would frequently show 100% of use, and it would not refresh automatically. Now it refreshes every 5 seconds. In order to reduce CPU load due to redrawing of the gauge, the client side now uses an HTML5 Canvas to redraw the gauge in the browser.   HardDrives: It was found that systems with large amounts of data in mail or recordings directories would spend an inordinate amount of time running du in order to report the disk usage by directory. Now this report is only run by explicit request and displays a warning that fetching the information will impact system performance. Also, some i18n work for Spanish.   PerformanceGraphic: i18n work.   CommunicationActivity: i18n work, and the network activity updates in real time. TelephonyHardware: i18n work. SQL injections closed. The webservice client now reports failures properly when the webservice cannot write the registration.

--- apps/core/system/modules/dashboard/themes/default/js/1_javascript.js
+++ apps/core/system/modules/dashboard/themes/default/js/1_javascript.js
@@ -1,372 +1,66 @@
-var module_name = "dashboard";
-
-$(document).ready(
-	function()
-	{
-        $(".column").sortable({
-            connectWith: ".column",
-            forcePlaceholderSize: true,
-            forceHelperSize: true,
-            scroll: false,
-            stop: function() { 
-                    var td_left  = document.getElementById("td_columns1");
-                    var td_right = document.getElementById("td_columns2");
-                    var children_left  = td_left.childNodes;
-                    var children_right = td_right.childNodes;
-                    var ids_applet = "";
-
-                    // Recorro los applet de la izquierda
-                    var j = 1;
-                    for(i=0; i<children_left.length;i++){
-                        if(children_left[i].nodeName == "DIV" || children_left[i].nodeName == "div"){
-                            var id_div = children_left[i].getAttribute("id");
-                            var tmp = id_div.split("-");
-                            if(tmp[0] == "applet"){
-                                var id_applet = tmp[2];
-                                ids_applet = ids_applet + id_applet + ":" + j + ",";
-                                j = j+2;
-                            }
-                        }
-                    }
-
-                    // Recorro los applet de la derecha
-                    j = 2;
-                    for(i=0; i<children_right.length;i++){
-                        if(children_right[i].nodeName == "DIV" || children_right[i].nodeName == "div"){
-                            var id_div = children_right[i].getAttribute("id");
-                            var tmp = id_div.split("-");
-                            if(tmp[0] == "applet"){
-                                var id_applet = tmp[2];
-                                ids_applet = ids_applet + id_applet + ":" + j + ",";
-                                j = j+2;
-                            }
-                        }
-                    }
-
-                    var order = 'menu=' + module_name + '&action=updateOrder&rawmode=yes&ids_applet=' + ids_applet;
-                    $.post("index.php", order,function(theResponse){});
-                }
-
-  });
-
-		// Toggle Single Portlet
-		/*$('a.toggle').click(function()
-			{
-				var p2 = $(this).parent('div');
-				var p3 = p2.parent('div').next('div').toggle();
-				var imgarrow = $(this).children("img").attr("src");
-				var id = $(this).children("img").attr("id");
-				var valor = changeArrow(imgarrow,id);
-				$(this).children("img").attr("src",valor);
-				return false;
-			}
-		);*/
-
-		// Invert All Portlets
-		$('a#all_invert').click(function()
-			{
-				$('div.portlet_content').toggle();
-				return false;
-			}
-		);
-
-		// Expand All Portlets
-		$('a#all_expand').click(function()
-			{
-				$('div.portlet_content:hidden').show();
-				arrowsExpand();
-				return false;
-			}
-		);
-
-		// Collapse All Portlets
-		$('a#all_collapse').click(function()
-			{
-				$('div.portlet_content:visible').hide();
-				arrowsCollapse();
-				return false;
-			}
-		);
-
-		// Open All Portlets
-		$('a#all_open').click(function()
-			{
-				$('div.portlet:hidden').show();
-				$('a#all_open:visible').hide();
-				$('a#all_close:hidden').show();
-				return false;
-			}
-		);
-
-		// Close All Portlets
-		$('a#all_close').click(function()
-			{
-				$('div.portlet:visible').hide();
-				$('a#all_close:visible').hide();
-				$('a#all_open:hidden').show();
-				return false;
-			}
-		);
-
-        // Applet admin
-        $('a#applet_admin,#close_applet_admin').click(function()
-            { // variable statusDivAppletAdmin declarada en tpl applet_admin
-                if(statusDivAppletAdmin=='open'){
-                    $('div.portlet:hide').show();
-                    $('a#all_close:hide').show();
-                    $('div#div_applet_admin:visible').hide();
-                    $('a#all_open:hide').show();
-                    statusDivAppletAdmin='closed';
-                }
-                else{
-                    $('div.portlet:visible').hide();
-                    $('a#all_close:visible').hide();
-                    $('div#div_applet_admin:hide').show();
-                    $('a#all_open:visible').hide();
-                    statusDivAppletAdmin='open';
-                }
-                return false;
-            }
-        );
-
-        $('.neo-applet-processes-row-menu').live('click', neoAppletProcesses_manejarMenu);
-	}
-);
-
-function saveRegister()
-{
-    var vendor = document.getElementById("manufacturer").value;
-    var num_se = document.getElementById("noSerie").value;
-    var id_card = $("#idCard").val();
-    var urlImaLoading = "<img src='images/loading.gif' height='20px' /><span style='font-size: 14px; position: relative; top: -10px; left: -5px; '></span>";
-   
-    if(vendor != "" && num_se != ""){
-        var order = 'menu=' + module_name + '&action=saveRegister&rawmode=yes&num_serie=' + num_se + '&hwd=' + id_card + '&vendor=' + vendor ;
-	 $('.loading').html(urlImaLoading);   
-	  
-        $.post("index.php", order,
-            function(theResponse){
-              //  alert("Card has been registered");
-	       $('.message').css('color', 'blue');
-               $('.message').html("Card has been Registered"); 
-	       $('.loading').html("");   
-               window.open("index.php?menu=dashboard","_self");
-        });
-    }
-    else{
-        //alert("The data input are blank");
-	 $('.message').css('color', 'red');
-	$('.message').html("The data input are blank"); 
-    }
-}
-
-function getDataCard()
-{
-    var id_card = $("#idCard").val();
-    var order = 'menu=' + module_name + '&action=getRegister&rawmode=yes&hwd=' + id_card;
-
-    $.post("index.php", order,
-        function(theResponse){
-            salida = theResponse.split(',');
-  	    $('#lman').css('padding-left',10);	
-	    $('#lser').css('padding-left',10);	
-	    $('#lman').append('<label>'+salida[0]+'</label>');
-            $('#lser').append('<label>'+salida[1]+'</label>');
-	   
-            
-		
-    });
-}
-
-function changeArrow(urlimg,id){
-  var sal = "";
-  var imgID = document.getElementById(id);
-  if(urlimg.indexOf('flecha_down.gif')!=-1){ 
-    sal = "modules/"+module_name+"/images/flecha_up.gif";
-  }
-  else{
-    sal = "modules/"+module_name+"/images/flecha_down.gif";
-  }
-  return sal;
-}
-
-function arrowsCollapse(){
-  for(var i=1; i<=12; i++){
-    var id = "imga"+i;
-    var imgID = document.getElementById(id);
-    imgID.src = "modules/"+module_name+"/images/flecha_down.gif";
-  }
-}
-
-function arrowsExpand(){
-  for(var i=1; i<=12; i++){
-    var id = "imga"+i;
-    var imgID = document.getElementById(id);
-    imgID.src = "modules/"+module_name+"/images/flecha_up.gif";
-  }
-}
-
-function loadAppletData()
-{
-    var arrAction          = new Array();
-    arrAction["menu"] = getCurrentElastixModule();
-    arrAction["action"]    = "loadAppletData";
-    arrAction["rawmode"]   = "yes";
-    request("index.php",arrAction,false,
-	function(arrData,statusResponse,error)
-	{
-	    if(statusResponse != "end"){
-		document.getElementById(arrData["code"]).innerHTML = arrData["data"];
-		loadAppletData();
-	    }
-	}
-    );
-}
-
-function jfunction(id)
-{
-    var arrID = id.split("_"); 
-    var a_id_card = arrID[1];
-    if(arrID[0]=="editMan1"){
-	var arrAction = new Array();
-		arrAction["menu"] = getCurrentElastixModule();
-    	arrAction["action"]  = "saveRegisterForm";
-    	arrAction["rawmode"] = "yes";
-	//var id = $(this).attr('id');
-    	request("index.php",arrAction,false,
-          function(arrData,statusResponse,error)
-          {
-  	      ShowModalPopUP(arrData['title'],285,120,arrData['html']);
-	      $("#idCard").val(a_id_card);
-          }
-   	 );
-    }//openWndMan1(a_id_card);
-    else{
-	var arrAction = new Array();
-		arrAction["menu"] = getCurrentElastixModule();
-    	arrAction["action"]  = "saveRegisterForm";
-    	arrAction["rawmode"] = "yes";
-	//var id = $(this).attr('id');
-    	request("index.php",arrAction,false,
-          function(arrData,statusResponse,error)
-          {
-  	      ShowModalPopUP(arrData['title'],290,60,arrData['html']);
-	      $("#idCard").val(a_id_card);
-	      $('.message').css('color', 'blue');
-	      $('.message').html(""); 
-	      $('.viewButton').remove();
-	      $('#manufacturer').remove();
-	      $('#noSerie').remove();	
-	      getDataCard();
-	      
-          }
-   	 );
-	}
-   
-}
-
-function refresh(element)
-{
-    var code = $(element).attr("id");
-    code = code.split("refresh_");
-    code = code[1];
-    var loading = $("#loading").val();
-    // Se obtiene la imagen loading con su texto traducido
-    $("#"+code).html("<img class='ima' src='modules/"+module_name+"/images/loading.gif' border='0' align='absmiddle' />&nbsp;"+loading);
-
-    // Se realiza la petición para obtener los datos del applet
-    var arrAction	 = new Array();
-    arrAction["menu"] = getCurrentElastixModule();
-    arrAction["action"]  = "refreshDataApplet";
-    arrAction["code"]    = code;
-    arrAction["rawmode"] = "yes";
-    request("index.php",arrAction,false,
-        function(arrData,statusResponse,error)
-        {
-            $("#"+code).html(arrData);
+$(document).ready(function() {
+	$('.appletcolumn').sortable({
+        connectWith: ".appletcolumn",
+        forcePlaceholderSize: true,
+        forceHelperSize: true,
+        scroll: false,
+        stop: function () {
+        	var appletlist_left = $('#applet_col_1 .appletwindow_content')
+        		.map(function() { return this.id; })
+        		.get();
+        	var appletlist_right = $('#applet_col_2 .appletwindow_content')
+        		.map(function() { return this.id; })
+        		.get();
+        	
+        	/* NOTA: el siguiente código codifica el orden de presentación de
+        	 * los applets pero no indica exactamente en qué columna aparecerán.
+        	 * Esto es consistente con la implementación anterior. */
+        	var appletlist = [];
+        	for (var i = 0; i < appletlist_left.length || i < appletlist_right.length; i++) {
+        		if (i < appletlist_left.length)
+        			appletlist.push(appletlist_left[i]);
+        		if (i < appletlist_right.length)
+        			appletlist.push(appletlist_right[i]);
+        	}
+        	
+        	//console.debug(appletlist);
+        	$.post('index.php', {
+        		menu: getCurrentElastixModule(),
+        		rawmode: 'yes',
+        		action: 'updateOrder',
+        		appletorder: appletlist
+        	}, function(respuesta) {
+        		if (respuesta.status == 'error') {
+        			alert(respuesta.message);
+        		}
+        	});
         }
-    );
-}
+	});
+	$('.appletrefresh').click(function() {
+		$(this).parent().parent().next().map(function() { appletRefresh($(this));});
+	});
 
-function neoAppletProcesses_esconderMenu()
+	// Iniciar la carga de todos los applets
+	$('.appletwindow_content').map(function() { appletRefresh($(this));});
+});
+
+function appletRefresh(appletwindow_content)
 {
-	$('.neo-applet-processes-menu').unbind('click');
-	$('html').unbind('click', neoAppletProcesses_esconderMenu);
-	$('.neo-applet-processes-menu').hide();
-	return false;
-}
-
-// Mostrar menú de administración en applet de procesos
-//function neoAppletProcesses_manejarMenu(divObject, sProc, sCurrState)
-function neoAppletProcesses_manejarMenu(event)
-{
-	sCurrState = $(this).children('#status-servicio').val();
-	isActivate = $(this).children('#activate-process').val();
-	sProc = $(this).children('#key-servicio').val();
-	if (sCurrState != 'OK' && sCurrState != 'Shutdown') return;
-	
-	if ($('.neo-applet-processes-menu').is(':visible')) {
-		neoAppletProcesses_esconderMenu();
-	} else {
-		event.stopPropagation();
-
-		// Operaciones para cerrar menú cuando se hace clic fuera
-		$('.neo-applet-processes-menu').click(function(event) {
-			event.stopPropagation();
-		});
-		$('html').click(neoAppletProcesses_esconderMenu);
-
-		// Se recuerda qué proceso se va a manejar
-		$('#neo_applet_selected_process').val(sProc);
-		
-		$('#neo-applet-processes-controles').show();
-		$('#neo-applet-processes-processing').hide();
-		
-		$('.neo_applet_process').unbind('click');
-		module_name = 'dashboard';
-		
-		$('.neo_applet_process').click(function() {
-			$('#neo-applet-processes-controles').hide();
-			$('#neo-applet-processes-processing').show();
-			$.post('index.php?menu=' + module_name + '&rawmode=yes', {
-				menu:		module_name, 
-				rawmode:	'yes',
-				action:		$(this).attr('name'),
-				process:	$('#neo_applet_selected_process').val()
-			},
-			
-			function (respuesta) {
-				neoAppletProcesses_esconderMenu();
-				refresh($('#refresh_Applet_ProcessesStatus').get(0));
-			});
-		});
-		
-		$('.neo-applet-processes-menu').show();
-		$('.neo-applet-processes-menu').css("position","fixed");
-		$('.neo-applet-processes-menu').position({
-			of: $(this),
-			my: "right top",
-			at: "right bottom",
-			offset: "-7 1"
-		});
-		
-		if (sCurrState == 'OK') {
-			$('#neo_applet_process_stop').show();
-			$('#neo_applet_process_restart').show();
-			$('#neo_applet_process_start').hide();
+	appletwindow_content.children('.appletwindow_fullcontent').hide().empty();
+	appletwindow_content.children('.appletwindow_wait').show();
+	$.post('index.php', {
+		menu: getCurrentElastixModule(),
+		rawmode: 'yes',
+		applet: appletwindow_content.attr('id').substr(7), // para quitar 'Applet_'
+		action: 'getContent'
+	}, function(respuesta) {
+		var fullcontent = appletwindow_content.children('.appletwindow_fullcontent');
+		appletwindow_content.children('.appletwindow_wait').hide();
+		if (respuesta.status == 'error') {
+			fullcontent.text(respuesta.message);
+		} else {
+			fullcontent.html(respuesta.html);
 		}
-		if (sCurrState == 'Shutdown') {
-			$('#neo_applet_process_stop').hide();
-			$('#neo_applet_process_restart').hide();
-			$('#neo_applet_process_start').show();
-		}
-		if(isActivate == '1')
-		{
-			$('#neo_applet_process_activate').hide();
-			$('#neo_applet_process_deactivate').show(); 
-		}else{
-			$('#neo_applet_process_activate').show();
-			$('#neo_applet_process_deactivate').hide();
-		}
-	}
+		fullcontent.show();
+	});
 }

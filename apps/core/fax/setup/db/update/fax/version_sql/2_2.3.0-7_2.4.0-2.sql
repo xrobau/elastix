40a920c4c052580f2ea3a625cd40d85caffa90d1
jbriones <jbriones@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-08-02 16:23:57 +0000

    UPDATED: Correction in a sql file to modify a registry in faxviewer.

--- apps/core/fax/setup/db/update/fax/version_sql/2_2.3.0-7_2.4.0-2.sql
+++ apps/core/fax/setup/db/update/fax/version_sql/2_2.3.0-7_2.4.0-2.sql
@@ -3,4 +3,5 @@ SET content = 'Fax sent from "{COMPANY_NAME_FROM}". The phone number is {COMPANY
 This email has a fax attached with ID {NAME_PDF}.
 Final status of fax job: {JOB_STATUS}'
 WHERE content = 'Fax sent from "{COMPANY_NAME_FROM}". The phone number is {COMPANY_NUMBER_FROM}.
-This email has a fax attached with ID {NAME_PDF}.';
+This email has a fax attached with ID {NAME_PDF}.
+Estado final del trabajo de fax: {JOB_STATUS}';

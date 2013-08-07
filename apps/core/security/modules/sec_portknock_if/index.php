1fd068fa4cedec814673a62fd2f320a49b7c61d9
wreyes <wreyes@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-08-07 18:15:18 +0000

    CHANGED: APPS - Core/Security/Modules/sec_portknock_if: code upgrade

--- apps/core/security/modules/sec_portknock_if/index.php
+++ apps/core/security/modules/sec_portknock_if/index.php
@@ -34,19 +34,13 @@ require_once "libs/paloSantoNetwork.class.php";
 
 function _moduleContent(&$smarty, $module_name)
 {
-    require_once "modules/$module_name/configs/default.conf.php";
-    require_once "modules/$module_name/libs/paloSantoPortKnockInterfaces.class.php";
-
-    load_language_module($module_name);
-
+    
     global $arrConf;
     global $arrConfModule;
     $arrConf = array_merge($arrConf, $arrConfModule);
 
     //folder path for custom templates
-    $base_dir = dirname($_SERVER['SCRIPT_FILENAME']);
-    $templates_dir = (isset($arrConf['templates_dir'])) ? $arrConf['templates_dir'] : 'themes';
-    $local_templates_dir = "$base_dir/modules/$module_name/".$templates_dir.'/'.$arrConf['theme'];
+    $local_templates_dir=getWebDirModule($module_name);
 
     $pDB = new paloDB($arrConf['dsn_conn_database']);
 

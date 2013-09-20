a9125be467373063b43f144618966cbe4bef1472
labarca2909 <labarca2909@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-09-20 19:23:20 +0000

    FIXED: An update line in the CDR table its corrected.

--- apps/core/reports/setup/db/update/asteriskcdrdb/version_sql/2_2.4.0-7_2.4.0-8.sql
+++ apps/core/reports/setup/db/update/asteriskcdrdb/version_sql/2_2.4.0-7_2.4.0-8.sql
@@ -4,4 +4,4 @@ alter table cdr add column `cnam`          varchar(40)  NOT NULL DEFAULT '';
 alter table cdr add column `outbound_cnum` varchar(40)  NOT NULL DEFAULT '';
 alter table cdr add column `outbound_cnam` varchar(40)  NOT NULL DEFAULT '';
 alter table cdr add column `dst_cnam`      varchar(40)  NOT NULL DEFAULT '';
-update cdr set recordingfile = userfield;
+update cdr set recordingfile = replace(userfield,'audio:','');

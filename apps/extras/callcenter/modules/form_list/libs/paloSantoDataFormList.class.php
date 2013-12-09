1e05c90ede6b2d4956b4b30aec82b639452ed556
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2013-12-09 22:53:34 +0000

    CHANGED: Form Preview: complete rewrite. Try to take advantage of the field generation of PaloSantoForm as much as possible. Remove dead xajax references.

--- apps/extras/callcenter/modules/form_list/libs/paloSantoDataFormList.class.php
+++ apps/extras/callcenter/modules/form_list/libs/paloSantoDataFormList.class.php
@@ -25,17 +25,14 @@
   | The Original Code is: Elastix Open Source.                           |
   | The Initial Developer of the Original Code is PaloSanto Solutions    |
   +----------------------------------------------------------------------+
-  $Id: formulario $ */
-//require_once("libs/js/jscalendar/calendar.php"); 
-require_once("libs/smarty/libs/Smarty.class.php");
-include_once("libs/paloSantoDB.class.php");
-/* Clase que implementa Formulario de Campanign de CallCenter (CC) */
-class paloSantoDataForm
+*/
+
+class paloSantoDataFormList
 {
-    var $_db; // instancia de la clase paloDB
+	private $_db;
     var $errMsg;
-    var $rutaDB;
-    function paloSantoDataForm($pDB)
+    
+    function __construct($pDB)
     {
         // Se recibe como parámetro una referencia a una conexión paloDB
         if (is_object($pDB)) {
@@ -52,166 +49,93 @@ class paloSantoDataForm
                 // debo llenar alguna variable de error
             }
         }
-
     }
-
-    function getFormularios($id_formulario = NULL,$estatus='all')
-    {
-        $arr_result = FALSE;
-        
-        $where = "";
-        if($estatus=='all')
-            $where .= "where 1";
-        else if($estatus=='A')
-            $where .= "where f.estatus='A'";
-        else if($estatus=='I')
-            $where .= "where f.estatus='I'";
-        if(!is_null($id_formulario))
-            $where .= " and f.id = $id_formulario";
-
-        if (!is_null($id_formulario) && !ereg('^[[:digit:]]+$', "$id_formulario")) {
-            $this->errMsg = _tr("Form ID is not valid");
-        } 
-        else {
-            $this->errMsg = "";
-            $sPeticionSQL = "SELECT f.id, f.nombre, f.descripcion, f.estatus FROM form f $where";
-            $arr_result =& $this->_db->fetchTable($sPeticionSQL, true);
-            if (!is_array($arr_result)) {
-                $arr_result = FALSE;
-                $this->errMsg = $this->_DB->errMsg;
-            }
-        }
-
-
-        return $arr_result;
-    }
-
-    function obtener_campos_formulario($id_formulario,$id_campo=NULL)
-    {
-        $respuesta = new xajaxResponse();
-        $smarty = $this->getSmarty();
-        $errMsg=""; 
-        $sqliteError='';
-        $arrReturn=array();
-        $where="";
-        $codigo_js="";
-        if(!is_null($id_campo))
-            $where = " and fd.id=$id_campo";
-
-        $query  = "
-                    SELECT  fd.id id_field, fd.etiqueta, fd.value value_field, fd.tipo, fd.orden, fd.id_form 
-                    FROM  form_field fd
-                    where fd.id_form = $id_formulario $where order by fd.orden";
-
-        $arr_fields = $this->_db->fetchTable($query, true);
-//print_r($arr_fields);
-        if (is_array($arr_fields) && count($arr_fields)>0) {
-
-            $id = $arr_fields[0]["id_form"];
-            foreach($arr_fields as $key=>$field) {
-                $funcion_js = "";
-                $input = $this->crea_objeto($smarty, $field, "", $funcion_js);
-                //echo $input;
-                $etiqueta = $field["etiqueta"];
-                $tipo = $field["tipo"];
-                $data_field[] = array("TYPE" => $tipo, "TAG" => $etiqueta, "INPUT" => $input, "ID_FORM" => $id);
-                //$data_field[] = array("TAG" => $etiqueta, "INPUT" => $input, "ID_FORM" => $id);
-                $id = "";
-                $codigo_js .= $funcion_js;
-                //echo $codigo_js;
-                $smarty->assign("FORMULARIO", $data_field);
-                $smarty->assign("formularios", _tr("Form"));
-                $mostrar_template=true;
-            }
-            if ($mostrar_template) $template = "formulario.tpl";
-            else $template = "vacio.tpl";
-        }else{
-            //$smarty->assign("no_definidos_formularios",_tr('Forms Nondefined'));
-            $template = "vacio.tpl";
-        }
-        if (isset($codigo_js) && trim($codigo_js)!="") {
-           $respuesta->addScript($codigo_js);
-        }
-        $texto_formulario=$smarty->fetch("file:/var/www/html/modules/form_list/themes/default/$template");
-        return $texto_formulario;
-        //$respuesta->addAssign("contenedor_formulario","innerHTML",$texto_formulario);
-        
-
-    }
-    function getSmarty() {
-    global $arrConf;
-    $smarty = new Smarty();
-    $smarty->template_dir = "themes/default/";
-    $smarty->compile_dir =  "var/templates_c/";
-    $smarty->config_dir =   "configs/";
-    $smarty->cache_dir =    "var/cache/";
-    return $smarty;
-    }
-
-    function crea_objeto(&$smarty, $field, $prefijo_objeto, &$funcion_js) {
-        $tipo_objeto = $field["tipo"];
-        $input="";
-        switch ($tipo_objeto) {
-            case "LIST":
-                $listado = explode(",",$field["value_field"]);
-                $input = "";
-                $selected="";
-                foreach($listado as $key=>$item) {
-                    //if ($field["value_data"] == $item) $selected = "selected";
-                    //else $selected="";
-                    if($item!="") $input .= "<option value='$item'>$item</option>";
-                }
-                if ($input!="") {
-                    $input = "<select name='$prefijo_objeto"."$field[id_field]' id='$prefijo_objeto"."$field[id_field]' class='SELECT'>$input</select>";
-                }
-            break;
-            case "DATE":
-                $input = $this->calendario("txt_".$field['id_field'],"btn_".$field['id_field']);
-            break;
-            case "TEXTAREA":
-                $input = "<textarea name='$prefijo_objeto"."$field[id_field]' id='$prefijo_objeto"."$field[id_field]' rows='3' cols='50'></textarea>";
-            break;
-            case "LABEL":
-                $input = "<label class='style_label'>$field[etiqueta]</label>";
-            break;
-            default:
-                $input = "<input type='text' name='$prefijo_objeto"."$field[id_field]' id='$prefijo_objeto"."$field[id_field]' value='' class='INPUT'>";
-        }
-
-        return $input;
-    }
-
-    function calendario($id_txt,$btn_txt) {
-
-    return 
-    "
-    <td>
-            <input name='name_$id_txt' id='$id_txt' type='text' 
-            style='width: 10em; color: #840; background-color: #fafafa; border: 1px solid #999999; text-align: center'/>
-            <a href='#' id='$btn_txt'>
-                <img align='middle' border='0' src='/libs/js/jscalendar/img.gif' alt='' />
-            </a>
-    </td>
-
-    <script type='text/javascript'>
-        Calendar.setup(
-            {
-                'ifFormat':'%Y-%m-%d',
-                'daFormat':'%Y-%m-%d',
-                'firstDay':1,
-                'showsTime':true,
-                'showOthers':true,
-                'timeFormat':24,
-                'inputField':'$id_txt',
-                'button':'$btn_txt'
-            }
-        );
-    </script> 
     
-    " ;
-    
+    private function _condSQL($status)
+    {
+        $param = array();
+        $where = array();
+        switch ($status) {
+        case 'all':
+            break;
+        case 'A':
+        case 'I':
+            $param[] = $status;
+            $where[] = 'estatus = ?';
+            break;
+        }
+        $cond = (count($where) > 0) ? ' WHERE '.implode(' AND ', $where) : '';
+        
+        return array($cond, $param);
     }
-
+    
+    function contarFormularios($status)
+    {
+        list($cond, $param) = $this->_condSQL($status);
+        $sql = 'SELECT COUNT(*) AS N FROM form'.$cond;
+        $tupla = $this->_db->getFirstRowQuery($sql, TRUE, $param);
+        if (!is_array($tupla)) {
+            $this->errMsg = $this->_db->errMsg;
+        	return FALSE;
+        }
+        return $tupla['N'];
+    }
+    
+    function listarFormularios($status, $limit = NULL, $offset = 0)
+    {
+    	list($cond, $param) = $this->_condSQL($status);
+        $sql = 'SELECT id, nombre, descripcion, estatus FROM form'.$cond;
+        if (!is_null($limit)) {
+        	$sql .= ' LIMIT ? OFFSET ?';
+            $param[] = $limit;
+            $param[] = $offset;
+        }
+        $recordset = $this->_db->fetchTable($sql, TRUE, $param);
+        if (!is_array($recordset)) {
+            $this->errMsg = $this->_db->errMsg;
+            return NULL;
+        }
+        return $recordset;
+    }
+    
+    function generarFormulario($id_form)
+    {
+    	$sql = 'SELECT nombre, descripcion, estatus FROM form WHERE id = ?';
+        $tupla = $this->_db->getFirstRowQuery($sql, TRUE, array($id_form));
+        if (!is_array($tupla)) {
+            $this->errMsg = $this->_db->errMsg;
+            return FALSE;
+        }
+        if (count($tupla) <= 0) return $tupla;
+        
+        $sql = 'SELECT id, etiqueta, value, tipo, orden FROM form_field WHERE id_form = ? ORDER by orden';
+        $recordset = $this->_db->fetchTable($sql, TRUE, array($id_form));
+        if (!is_array($recordset)) {
+            $this->errMsg = $this->_db->errMsg;
+            return NULL;
+        }
+        $form = array();
+        foreach ($recordset as $tuplacampo) {
+        	$field = array(
+                'LABEL'                     =>  $tuplacampo['etiqueta'],
+                'REQUIRED'                  => 'no',
+                'INPUT_TYPE'                =>  $tuplacampo['tipo'],
+                'INPUT_EXTRA_PARAM'         => '',
+                'VALIDATION_TYPE'           =>  'text',
+                'VALIDATION_EXTRA_PARAM'    =>  '',
+            );
+            if ($tuplacampo['tipo'] == 'LIST') {
+            	$field['INPUT_TYPE'] = 'SELECT';
+                $field['INPUT_EXTRA_PARAM'] = array();
+                foreach (explode(',', $tuplacampo['value']) as $v) {
+                	$field['INPUT_EXTRA_PARAM'][$v] = $v;
+                }
+            }
+            $form[$tuplacampo['etiqueta']] = $field;
+        }
+        $tupla['campos'] = $form;
+        return $tupla;
+    }
 }
 
 ?>

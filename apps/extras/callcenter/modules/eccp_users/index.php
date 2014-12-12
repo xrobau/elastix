005a046eeffa1018e3a9afc0a03aa77238dffc2c
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2014-12-12 17:55:12 +0000

    CHANGED: ECCP Users: update paloSantoGrid API usage to latest Elastix 2.4

--- apps/extras/callcenter/modules/eccp_users/index.php
+++ apps/extras/callcenter/modules/eccp_users/index.php
@@ -114,17 +114,14 @@ function listarUsuarios($pDB, $smarty, $module_name, $local_templates_dir)
                      "icon"     => 'images/user.png',
                      "width"    => "99%",
                      "columns"  => array(
-                                        0 => array("name"       => 
-                                            '<input type="submit" class="button" name="delete" value="'.
-                                            htmlentities(_tr('Delete'), ENT_COMPAT, 'UTF-8').
-                                            '" onclick="return confirmSubmit('."'".
-                                                htmlentities(_tr('Are you sure to delete this user?'), ENT_COMPAT, 'UTF-8')."'".')" />'),
+                                        0 => array("name"       => ''),
                                         1 => array("name"       => _tr('Name')),
                                         2 => array("name"       => _tr('Options')),
                                         )
                     );
     
     $oGrid->addNew("?menu=$module_name&action=new_user", _tr('New ECCP User'), true);
+    $oGrid->deleteList('Are you sure to delete this user?', 'delete', _tr('Delete'));
     $sContenido = $oGrid->fetchGrid($arrGrid, $arrData,$arrLang);
     if (strpos($sContenido, '<form') === FALSE)
         $sContenido = "<form  method=\"POST\" style=\"margin-bottom:0;\" action=\"$url\">$sContenido</form>";
